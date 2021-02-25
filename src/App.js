bc61f17f022a7a9a16922fbe5b3376c4fb48ad44
razhui <56419009+razhui@users.noreply.github.com>
2021-02-24 22:42:13 -0800

    add responsive sidenav

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ function App() {
   return (
     <div className="App">
       <header>
-
-      </header>
-      <main>
       <div class="sidenav">
         <a href="#">1</a>
         <a href="#">2</a>
         <a href="#">3</a>
       </div>
-        <h1>test</h1>
-      </main>
+      </header>
+      <body>
+        <div class="content">
+            <h1>The standard Lorem Ipsum passage, used since the 1500s</h1>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        </div>
+      </body>
     </div>
   );
 }
